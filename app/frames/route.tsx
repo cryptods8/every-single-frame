6d719b36a6beea445718851d6699645e3b1b7491
cryptods8 <dusoo.s+ds8@gmail.com>
2024-08-27 08:44:12 +0200

    frame message validation

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -1,10 +1,46 @@
 import { createFrames, Button } from "frames.js/next";
 import data from "../data.json";
 import { baseUrl } from "../constants";
+import { validateFrameMessage } from "frames.js";
+import { FramesMiddleware } from "frames.js/types";
+
+const airstackApiKey = process.env["AIRSTACK_API_KEY"];
+
+const hubHttpUrl = airstackApiKey
+  ? "https://hubs.airstack.xyz"
+  : process.env["FRAME_HUB_HTTP_URL"] || "http://localhost:3010/hub";
+const hubRequestOptions = airstackApiKey
+  ? {
+      headers: { "x-airstack-hubs": airstackApiKey },
+    }
+  : undefined;
+
+const validationMiddleware: FramesMiddleware<any, {}> = async (ctx, next) => {
+  const { request } = ctx;
+  if (request.method !== "POST") {
+    return next();
+  }
+
+  request
+    .clone()
+    .json()
+    .then((payload) =>
+      validateFrameMessage(payload, {
+        hubHttpUrl,
+        hubRequestOptions,
+      })
+    )
+    .catch((error) => {
+      console.error("validation error", error);
+    });
+
+  return next();
+};
 
 const frames = createFrames({
   baseUrl,
   basePath: "/frames",
+  middleware: [validationMiddleware],
 });
 
 const handleRequest = frames(async (ctx) => {
